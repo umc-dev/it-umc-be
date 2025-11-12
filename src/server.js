74dd721cc42c19455f0a966c56fb9b4684332915
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2025-11-12 16:22:34 +0700

    fix : fix prisma and update model

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).json({
     message: `API is running securely with Helmet on ${env.NODE_ENV} mode`,
   });
