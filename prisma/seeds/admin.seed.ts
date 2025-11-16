13faae2e04d3dfa7f94efcc1bee51611b2fac665
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2025-11-16 11:13:45 +0700

    feat(database): use bahasa indonesia faker and add script to reset and seed

--- prisma/seeds/admin.seed.ts
+++ prisma/seeds/admin.seed.ts
@@ -1,17 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-import { faker } from "@faker-js/faker";
+import { Faker, id_ID } from "@faker-js/faker";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 export async function seedAdmins() {
+  // Total data
   const totalAdmins = 50;
+
+  // Faker Bahasa Indonesia
+  const fakerID = new Faker({ locale: [id_ID] });
 
   const adminsData = await Promise.all(
     Array.from({ length: totalAdmins }).map(async () => ({
-      name: faker.person.fullName(),
-      email: faker.internet.email().toLowerCase(),
-      password: await bcrypt.hash(faker.internet.password(), 10),
+      name: fakerID.person.fullName(),
+      email: fakerID.internet.email().toLowerCase(),
+      password: await bcrypt.hash(fakerID.internet.password(), 10),
       createdAt: new Date(),
       updatedAt: new Date(),
     })),
