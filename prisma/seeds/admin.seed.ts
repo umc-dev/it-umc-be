8e1cf28174a3a9b45d4154f0a01cc40b638d319a
frizapc <frizamuhammad99@gmail.com>
2025-11-21 16:10:10 +0700

    build(seeder): use 1 admin data manually for postman auth api test

--- prisma/seeds/admin.seed.ts
+++ prisma/seeds/admin.seed.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 
 export async function seedAdmins() {
   // Total data
-  const totalAdmins = 5;
+  const totalAdmins = 1;
 
   // Faker Bahasa Indonesia
   const fakerID = new Faker({ locale: [id_ID] });
@@ -14,7 +14,7 @@ export async function seedAdmins() {
   const adminsData = await Promise.all(
     Array.from({ length: totalAdmins }).map(async () => ({
       name: fakerID.person.fullName(),
-      email: fakerID.internet.email().toLowerCase(),
+      email: "admin@mail.com",
       password: await bcrypt.hash("cirebon321", 10),
       createdAt: new Date(),
       updatedAt: new Date(),
