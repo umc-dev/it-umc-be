92d9a42b8c1760cebe8fba60f5b5a5c6d1e6c9c8
frizapc <frizamuhammad99@gmail.com>
2025-12-05 07:44:05 +0700

    feat(database): add schema StatisticStudent

--- prisma/migrations/20251205004043_add_schema_for_statistic_student/migration.sql
+++ prisma/migrations/20251205004043_add_schema_for_statistic_student/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `statistic_student` (
+CREATE TABLE `StatisticStudent` (
     `id` VARCHAR(191) NOT NULL,
     `tahun` MEDIUMINT UNSIGNED NOT NULL,
     `total_keluar` MEDIUMINT UNSIGNED NOT NULL,
@@ -7,6 +7,6 @@ CREATE TABLE `statistic_student` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `statistic_student_tahun_key`(`tahun`),
+    UNIQUE INDEX `StatisticStudent_tahun_key`(`tahun`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
