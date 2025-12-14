dcad2e3c0688b1772c49dc6fad39eba04705f444
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2025-12-14 11:10:09 +0700

    feat(auth) : add /me and /logout endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,17 @@
 import { IRouter, Router } from "express";
 import authController from "../controllers/auth.controller";
 import passport from "passport";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const authRouter: IRouter = Router();
 
+// Login with email
 authRouter.post("/", authController.loginWithEmail);
 
+// Get authenticated user
+authRouter.get("/me", authMiddleware, authController.me);
+
+// Login with google
 authRouter.get(
   "/google",
   passport.authenticate("google", {
@@ -13,10 +19,14 @@ authRouter.get(
   }),
 );
 
+// Google oauth callback
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", { session: false }),
   authController.loginWithGoogle,
 );
+
+// Logout
+authRouter.post("/logout", authController.logout);
 
 export default authRouter;
