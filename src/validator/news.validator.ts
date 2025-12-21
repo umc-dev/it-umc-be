8f4b445a3d80121f235bf77500cdbb32c39f256a
frizapc <frizamuhammad99@gmail.com>
2025-12-21 21:00:17 +0700

    fix(news): change categoryId type from string to number

--- src/validator/news.validator.ts
+++ src/validator/news.validator.ts
@@ -3,11 +3,11 @@ import { z } from "zod";
 export const CreateNewsSchema = z.object({
   title: z.string().min(1, "Title is required"),
   content: z.string().min(1, "Content is required"),
-  categoryId: z.string().min(1, "Category ID is required"),
+  categoryId: z.number().min(1, "Category ID is required"),
 });
 
 export const UpdateNewsSchema = z.object({
   title: z.string().optional(),
   content: z.string().optional(),
-  categoryId: z.string().optional(),
+  categoryId: z.number().optional(),
 });
