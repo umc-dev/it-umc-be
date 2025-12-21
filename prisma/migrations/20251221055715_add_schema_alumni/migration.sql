80a98515359cf84ec72eab1b930f163290f5c4f7
frizapc <frizamuhammad99@gmail.com>
2025-12-21 13:01:17 +0700

    fix(migration): delete unique key for alumni.year

--- prisma/migrations/20251221055715_add_schema_alumni/migration.sql
+++ prisma/migrations/20251221055715_add_schema_alumni/migration.sql
@@ -8,6 +8,5 @@ CREATE TABLE `Alumni` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Alumni_year_key`(`year`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
