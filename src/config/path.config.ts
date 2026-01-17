f307ddda340fc6ef295badf7dd044eff68a13561
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2026-01-17 11:47:36 +0700

    fix(uploads) : fix uploads path to support in container docker

--- src/config/path.config.ts
+++ src/config/path.config.ts
@@ -4,7 +4,5 @@ import { env } from "./env";
 export const ROOT_PATH = process.cwd();
 
 export const UPLOADS_PATH = env.UPLOADS_PATH
-  ? path.isAbsolute(env.UPLOADS_PATH)
-    ? env.UPLOADS_PATH
-    : path.join(ROOT_PATH, env.UPLOADS_PATH)
-  : path.join(ROOT_PATH, "uploads");
+  ? path.resolve(env.UPLOADS_PATH)
+  : "/uploads";
